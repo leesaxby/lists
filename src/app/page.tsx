bd8844be1287bdffec37efda29f9d06d2bc918e5
leesaxby <lee.saxby82@gmail.com>
2025-06-11 12:24:04 +0100

    remove unused session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import styles from "./page.module.css";
 import SignIn from '@/ui/sign-in';
 import SignOut from '@/ui/sign-out';
-import { auth } from '@/auth';
 
 export default async function Home() {
-  const session = await auth();
 
   return (
     <div className={styles.page}>
